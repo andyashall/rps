65064a395614be58d33c934e6d6633f9b1bbfb60
andy <andy@ashall.co>
2017-09-18 12:57:45 +0100

    done using randomR

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,15 @@ main = do
   putStrLn "Rock, Paper or Scissors?"
   o <- getLine
   g <- newStdGen
-  let d = randomRIO (1, 3) :: IO (Int)
+  let d = randomR (1, 3) g :: (Int, StdGen)
   let l = lowerString o
   let i = toInt l
   let r = compareS i d
-  print r
+  let m = result r i d
+  print m
 
-compareS :: Int -> IO Int -> Bool
-compareS o d = o == d
+compareS :: Int -> (Int, StdGen) -> Bool
+compareS o (d,x) = o == d
 
 toInt :: String -> Int
 toInt s = case s of
@@ -23,7 +24,16 @@ toInt s = case s of
   "paper" -> 2
   "scissors" -> 3
 
+toStr :: Int -> String
+toStr i = case i of
+  1 -> "Rock"
+  2 -> "Paper"
+  3 -> "Scissors"
+
 lowerString :: [Char] -> String
 lowerString str = [ toLower loweredString | loweredString <- str]
 
--- getRan :: 
+result :: Bool -> Int -> (Int, StdGen) -> String
+result r i (d,x) = case r of
+  True -> "You win! Dealer was: " ++ (show $ toStr d)
+  False -> "You loose! Dealer was: " ++ (show $ toStr d)
